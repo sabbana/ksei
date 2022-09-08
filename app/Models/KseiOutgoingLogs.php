73ae5789df56d90021460648cdaad4b378ec6985
sabbana <sabbana.a7@gmail.com>
2022-09-09 03:41:09 +0700

    fix: model attribute

--- app/Models/KseiOutgoingLogs.php
+++ app/Models/KseiOutgoingLogs.php
@@ -17,7 +17,6 @@ class KseiOutgoingLogs extends Model
         "method_ksei",
         "json_request",
         "xml_request",
-        "response",
         "status",
         "external_reference",
         "no_cif",
